97a6430119e7bed5099414dd4566456be6dd9f74
Eric Mill <eric@konklone.com>
2021-09-06 23:59:07 -0400

    a working scroll highlight effect on the sidenav

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,54 @@
 console.log("");
 
 
+// assume sidenav headers maps to all in-doc headers
+var headers = [];
+var headerNames = [];
+var sidenavs = document.querySelectorAll(".usa-sidenav__item a");
+
+sidenavs.forEach(function(link) {
+  var name = link.getAttribute("href").substring(1);
+  headerNames.push(name);
+  headers.push(document.getElementById(name));
+});
+
+// keep track of current one
+var closestHeader;
+
+// Find the header that is just above (or at the top) of the viewport,
+// and update the corresponding sidenav link to be styled as current.
+function calculateCurrentHeader(event) {
+
+  // This number is where we'll call the "top" - anything above that is active
+  var pageTop = 200;
+  var closestHeaderTop = -9999;
+  // default to topmost header (even if it starts below)
+  var currentClosest = headers[0];
+
+  headers.forEach(function(header) {
+    var top = header.getBoundingClientRect().top;
+    if (top > closestHeaderTop && top < pageTop)
+      currentClosest = header;
+  });
+
+
+  if (closestHeader != currentClosest) {
+
+    // out with the old current nav
+    var currentCurrent = document.querySelector(".usa-sidenav__item a.usa-current");
+    if (currentCurrent) currentCurrent.classList.remove("usa-current");
+
+    // long live the new current nav
+    var newCurrent = document.querySelector(".usa-sidenav__item a[href='#" + currentClosest.id + "']");
+    if (newCurrent) newCurrent.classList.add("usa-current");
+
+    closestHeader = currentClosest;
+  }
+}
+
+
+// run once on load
+calculateCurrentHeader();
+
+// run as you scroll
+window.addEventListener("scroll", calculateCurrentHeader);
